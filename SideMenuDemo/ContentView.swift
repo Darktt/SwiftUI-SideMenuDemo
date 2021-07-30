7d661226f79b33f729d4a741c755e44079ebb86c
Eden <edendudoo@gmail.com>
2021-07-30 09:44:02 +0800

    Initial Commit

---
+++ SideMenuDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SideMenuDemo
+//
+//  Created by Eden on 2021/7/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SideMenuDemo/SideMenuDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuDemoApp.swift
+//  SideMenuDemo
+//
+//  Created by Eden on 2021/7/30.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
